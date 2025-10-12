321f4bbce1cc35a7997178ed2dfa9f74de83a4af
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-12 20:46:13 +0300

    Update main.py

---
+++ task_2/main.py
@@ -0,0 +1,9 @@
+#Темник Кирилл
+
+str1 = input("Введите строку 1: ")
+str2 = input("Введите строку 2: ")
+
+if sorted(str1.replace(" ", "").lower()) == sorted(str2.replace(" ", "").lower()):
+    print("Явл")
+else:
+    print("Нет")
