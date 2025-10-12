c5d60d79077bfece6e579c32355855a78bfb20ee
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-12 22:02:55 +0300

    Update main.py

---
+++ task_5/main.py
@@ -0,0 +1,12 @@
+#Темник Кирилл
+
+str = input("Введите строку: ")
+
+count_of_vowels = 0
+vowels = "аоэеиыуёюяАОЭЕИЫУЁЮЯaeiouAEIOU"
+
+for i in str:
+    if i in vowels:
+        count_of_vowels = count_of_vowels + 1
+        
+print("Количество гласных = ", count_of_vowels)
