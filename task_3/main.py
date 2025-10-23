3802eeb3c57a2ff5cb6eb09c65a5e4b50c111025
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-23 22:07:22 +0300

    Update main.py

--- task_3/main.py
+++ task_3/main.py
@@ -1,8 +1,9 @@
 #Кирилл
 
-f = open(r"text.txt","r",encoding = 'UTF-8')
-text = f.read()
-count_of_words = text.split()
+f = open(r"text.txt","r",encoding = 'UTF-8')# Открываем файл "text.txt" для чтения
+text = f.read()# Читаем файл
+count_of_words = text.split()# В переменную count_of_words записываем количество слов из файла "text.txt"
 
-print(count_of_words)
+print(count_of_words)# Выводим значение переменной count_of_words
+
 
