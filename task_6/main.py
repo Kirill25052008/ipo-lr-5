9c9afa0766f8375d9c1fd1dc653af489e5c9c674
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-23 22:31:53 +0300

    Update main.py

--- task_6/main.py
+++ task_6/main.py
@@ -1,14 +1,16 @@
-#Кирилл
+# Кирилл
+# Вариант 3
 
-f = open(r"text.txt", "r", encoding = 'UTF-8')
-text = f.read()
-count_of_words = text.lower().split()
-count_of_new_words = sorted(set(count_of_words))
+f = open(r"text.txt", "r", encoding = 'UTF-8')# Открываем файл "text.txt" для чтения
+text = f.read()# В переменную text записываем текст из файла "text.txt"
+count_of_words = text.lower().split()# В переменную count_of_words (количество слов) записываем все слова из файла "text.txt" в нижнем регистре
+count_of_new_words = sorted(set(count_of_words))# В переменную count_of_new_words (количество новых слов) записываем все уникальные слова из переменной count_of_words (количество слов)
 
-print(count_of_new_words)
+print(count_of_new_words)# Выводим значение переменной count_of_words (количество слов)
 
-q = open(r"output.txt", "w", encoding = 'UTF-8')
+q = open(r"output.txt", "w", encoding = 'UTF-8')# Открываем новый файл "output.txt" для записи
 
-for i in count_of_new_words:
-    q.write(i + ", ")
+for i in count_of_new_words:# Создаём цикл (пока переменная i находится в переменной count_of_words (количество слов))
+    q.write(i + ", ")# Записываем все уникальные слова из переменной count_of_words (количество слов) в новый файл "output.txt"
+
 
